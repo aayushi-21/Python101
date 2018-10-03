31d427f4c408ef9c91e965f419fa620f2b1182c5
Aayushi Agrawal <y13uc007@lnmiit.ac.in>
2018-10-03 17:39:01 -0400

    Final Changes in dom.py

--- w5/dom.py
+++ w5/dom.py
@@ -1,40 +1,86 @@
 from num import Num
 from row import rows
-
+import random, math
+import re, traceback
 samples = 100
 
+class O():
+    y = n = 0
 
-def dom(self, row1, row2):
+    @staticmethod
+    def report():
+        print("\n# pass= %s fail= %s %%pass = %s%%" % (
+            O.y, O.n, int(round(O.y * 100 / (O.y + O.n + 0.001)))))
+
+    @staticmethod
+    def k(f):
+        try:
+            print("\n-----| %s |-----------------------" % f.__name__)
+            if f.__doc__:
+                print("# " + re.sub(r'\n[ \t]*', "\n# ", f.__doc__))
+            f()
+            print("# pass")
+            O.y += 1
+        except:
+            O.n += 1
+            print(traceback.format_exc())
+        return f
+
+
+def cp(i, l, u):
+    if i in range(l, u):
+        return input
+    elif i >= u:
+        return u
+    elif i <= l:
+        return l
+
+def another(x, t):
+    y = cp(math.floor(0.5 + random.random() * len(t)), 0, len(t) - 1)
+    if x == y:
+        return another(x, t)
+    if t[y]:
+        return t[y]
+    return another(x, t)
+
+def dom(data, row1, row2):
     s1 = 0
     s2 = 0
-    n = 0
-
-    for _ in enumerate(self.w):
-        n = n + 1
-
-    for c, w in enumerate(self.w):
+    n = (len(data.w))
+    for c, w in enumerate(data.w):
         a0 = row1[c]
         b0 = row2[c]
-        a = Num.numNorm(self.nums[c], a0)
-        b = Num.numNorm(self.nums[c], b0)
+        a = Num.numNorm(data.nums[c], a0)
+        b = Num.numNorm(data.nums[c], b0)
         s1 = s1 - 10 ^ (w * (a - b) / n)
         s2 = s2 - 10 ^ (w * (b - a) / n)
-
     return s1 / n < s2 / n
 
 
-def doms(t):
+def doms(data):
     n = samples
-    c = len(t.name) + 1
-    print(t.name + "," + ",>dom")
-    for row1 in t.rows:
+    c = len(data.name)
+    new_header = data.name + ['>dom']
+    for r1, row1 in enumerate(data.rows):
         row1.append(0)
-        for row2 in t.rows:
-            if (row1 == row2):
-                continue
-            s = dom(t, row1, row2) and 1 / n or 0
-            row1[-1] = row1[-1] + s
-
+        for i in range(n):
+            row2 = another(r1, data.rows)
+            s = dom(data, row1, row2) and 1 / n or 0
+            row1[c] += s
+    new_header = ' '.join(new_header)
+    print(new_header)
+    all_rows = data.rows
+    for s in all_rows:
+        print(*s)
 
 def mainDom(source):
+    print(source)
     doms(rows(source))
+
+@O.k
+def domTest1():
+        mainDom("Weather.csv")
+        
+@O.k
+def domTest2():
+        mainDom("auto.csv")
