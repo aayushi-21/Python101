75b8d6353e881edfa1b9fe2febbe5246d450eed5
Aayushi Agrawal <y13uc007@lnmiit.ac.in>
2018-10-03 18:27:24 -0400

    Creating row.py for Week 6

---
+++ w6/row.py
@@ -0,0 +1,89 @@
+import re
+import traceback
+from sym import Sym
+from num import Num
+import sys
+import re
+from tabulate import tabulate
+
+class Data:
+    def __init__(self):
+        self.w = {}
+        self.syms = {}
+        self.nums = {}
+        self._class = None
+        self.rows = []
+        self.name = []
+        self._use = []
+        self.indeps = []
+
+    def indep(self, c):
+        return c not in self.w and self._class != c
+
+    def dep(self, c):
+        return not self.indep(c)
+
+    def header(self, cells):
+        for i, v in enumerate(cells):
+            if not re.match(r'^\?', v):
+                c = len(self._use)
+                self._use.append(i)
+                self.name.append(v)
+                if re.search('[<>$]', v):
+                    self.nums[c] = Num([])
+                else:
+                    self.syms[c] = Sym()
+                if re.search('<', v):
+                    self.w[c] = -1
+                elif re.search('>', v):
+                    self.w[c] = 1
+                elif re.search('!', v):
+                    self._class = c
+                else:
+                    self.indeps.append(c)
+        return self
+
+    def row(self, cells):
+        r = len(self.rows)
+        self.rows.append([])
+        for c, c0 in enumerate(self._use):
+            x = cells[c0]
+            if x != '?':
+                if self.nums.get(c) is not None:
+                    self.nums[c].numInc(float(x))
+                else:
+                    self.syms[c].symInc(x)
+            self.rows[r].append(x)
+        return self
+
+
+def rows1(src):
+    data = Data()
+    first = True
+    for line in src:
+        line = re.sub('[\t\r\n]*|#.*', "", line)
+        cells = [i.strip() for i in line.split(',')]
+        if len(cells) > 0:
+            if first:
+                data.header(cells)
+            else:
+                data.row(cells)
+            first = False
+    return data
+
+
+def lines(src=None):
+    if src == None:
+        for line in sys.stdin:
+            yield line
+    elif src[-3:] in ["csv", ".dat"]:
+        with open(src) as fs:
+            for line in fs:
+                yield line
+    else:
+        for line in src.splitlines():
+            yield line
+
+
+def rows(s):
+    return rows1(lines(s))
